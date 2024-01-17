5ff60a6af59fa7c683777190eeec2bce67768610
harshadachame <harshadachame71@gmail.com>
2024-01-17 18:24:58 +0530

    Second Commit

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object{
+       const val API_KEY = "cfc5864358014f1d8b0b0749f8691760"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE =20
+    }
+}
